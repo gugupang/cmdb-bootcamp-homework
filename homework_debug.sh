eb204f0ea3dc69840312b8107eb5e7a27a9f080c
Liang-Yu <gugulypang@gmail.com>
2014-09-04 13:19:19 -0400

    m

---
+++ homework_debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Day 1 - Homework: Part 2 - debug this bash script
+#
+
+echo "There are around 6 mistakes"
+
+FASTQ_DIR=/Users/cmdb/data/fastq
+OUTPUT_DIR=/Users/cmdb/data/day1
+SAMPLE_PREFIX=SRR072
+
+GENOME_DIR=/Users/cmdb/data/genomes
+GENOME_INDEX_BASE=dmel-all-chromosome-r5.57
+ANNOTATION=dmel-all-r5.57.gff
+
+CORES=4
+
+for i in $(seq 893 916)
+do
+  echo fastqc -o $OUTPUT_DIR $FASTQ_DIR/$SAMPLE_PREFIX$i.fastq.gz 
+  echo tophat -p$CORES -G $GENOME_DIR/$ANNOTATION -o $SAMPLE_PREFIX$i\_tophat --no-novel-junc --segment-length 20 $GENOME_DIR/$GENOME_INDEX_BASE $FASTQ_DIR/$SAMPLE_PREFIX$i.fastq
+  echo cufflinks -p$CORES -G $GENOME_DIR/$ANNOTATION -o $SAMPLE_PREFIX$i\_cufflinks $OUTPUT_DIR/$SAMPLE_PREFIX$i/accepted_hits.bam
+done
