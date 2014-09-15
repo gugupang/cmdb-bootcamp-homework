29920930c5bf12ce914b0c8df5d1d9dd83c0b39e
Liang-Yu <gugulypang@gmail.com>
2014-09-14 22:16:59 -0400

    day2 luch

---
+++ day2_luch/day2_luch_exc_4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+Cufflinks_output =  "/Users/cmdb/data/day1/Tophat_output/accepted_hits_no_head.sam"
+
+f = open(Cufflinks_output)
+
+for l in f:
+    column = f.split("\t")
+print column[2]
+    
+    
+         
+        
+            
+
+
+        
